3302a2d6658112cc6e049a5ae7e0f4cd12a60dbf
jimmyarbats <jimmyaraujob@gmail.com>
2023-12-05 19:37:46 -0300

    quiz project done

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Clicou");
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
